44cdbc147820f79fd5eaeddeb3b605e6c46142df
Zoolah <jadellanes97@gmail.com>
2017-10-31 15:05:39 +0100

    ajout README

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//En commentaire : ce qu'a fait le prof
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  titre: String;
+  app: App;
 
+  constructor(public navCtrl: NavController) {
+    this.titre = "Titre";
+    this.app = new App(this.titre, 0.1);
   }
 
+  Declencheur(){
+    console.log("c'est ok");
+  }
+}
+
+class App {
+  nom: String;
+  version: Number;
+
+  constructor(nom: String, version: Number){
+    this.nom = nom;
+    this.version = version;
+  }
 }
